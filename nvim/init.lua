856a4303468e5f92ab9805d480e6f6bb9234b646
Juan Vidal Allende <juanvidalallende@gmail.com>
2025-02-25 12:36:43 +0100

    Basic configuration for neovim
     - Line numbers
     - Horizontal/vertical bars
     - Search options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,23 @@
 require("config.lazy")
 
+-- Reduce boilerplate
+local opt = vim.opt
+
 -- Setup centralized directories for swap, undo, and backup
 local prefix = vim.env.XDG_CONFIG_HOME or vim.fn.expand("~/.config")
-vim.opt.directory = { prefix .. "/nvim/.swp//"}
-vim.opt.undodir = { prefix .. "/nvim/.undo//"}
-vim.opt.backupdir = {prefix .. "/nvim/.backup//"}
+opt.directory = { prefix .. "/nvim/.swp//"}
+opt.undodir = { prefix .. "/nvim/.undo//"}
+opt.backupdir = {prefix .. "/nvim/.backup//"}
+
+-- Show line numbers
+opt.number = true
+
+-- Cursor line
+opt.cursorline = true
+-- opt.cursorlineopt = "number"
+opt.colorcolumn = "80,100"
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
